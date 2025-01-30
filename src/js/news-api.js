d2b2f5245267d743ec80fdc3f2009d3516726686
Oleksandr Kostiuchenko <aleksandrkostucenko11@gmail.com>
2025-01-30 20:55:35 +0100

    fix-fetech-version

--- src/js/news-api.js
+++ src/js/news-api.js
@@ -5,12 +5,7 @@ import axios from "axios";
 
 //* Export fetch function
 export const fetchNews = async (userQuery, page) => {
-    const galleryData = await axios.get(`https://newsapi.org/v2/everything?q=${userQuery}&apiKey=ee921bec82754786b568b8b5c41f6003&pageSize=15&page=${page}`,{
-        headers: {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-        }
-    }
-    );
+    const galleryData = await axios.get(`https://newsapi.org/v2/everything?q=${userQuery}&apiKey=ee921bec82754786b568b8b5c41f6003&pageSize=15&page=${page}`);
 
     return galleryData;
 }
